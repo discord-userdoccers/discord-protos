273a2d4b2967ba91ec3d3230452a8aba299152ea
dolfies <jeyalfie47@gmail.com>
2026-03-06 04:25:27 -0500

    fix: puppeteer is a piece of shit

--- src/load.ts
+++ src/load.ts
@@ -97,6 +97,7 @@ interface Proto {
 async function main() {
     const browser = await puppeteer.launch({
         args: ["--no-sandbox", "--disable-setuid-sandbox", "--disable-dev-shm-usage"],
+        executablePath: puppeteer.executablePath(),
     });
 
     const page = await browser.newPage();
